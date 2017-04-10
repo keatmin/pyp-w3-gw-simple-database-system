07c15c7f4efda7e6338771fcb7404f13ddb7eb08
Keat <keatmin3@gmail.com>
2017-04-10 03:47:46 +0000

    Initial commit

--- simple_database/main.py
+++ simple_database/main.py
@@ -1,5 +1,6 @@
 import os
 from datetime import date
+import json
 
 from simple_database.exceptions import ValidationError
 from simple_database.config import BASE_DB_FILE_PATH
@@ -20,85 +21,114 @@ class Table(object):
         self.table_filepath = os.path.join(BASE_DB_FILE_PATH, self.db.name,
                                            '{}.json'.format(self.name))
 
-        # In case the table JSON file doesn't exist already, you must
-        # initialize it as an empty table, with this JSON structure:
-        # {'columns': columns, 'rows': []}
+        if not os.path.exists(self.table_filepath):
+            with open(self.table_filepath, 'a') as fp:
+                fp.write(json.dumps({'columns': columns, 'rows': []}))
 
         self.columns = columns or self._read_columns()
 
     def _read_columns(self):
         # Read the columns configuration from the table's JSON file
         # and return it.
-        pass
+        with open(self.table_filepath, 'r') as fp:
+            return json.load(fp)['columns']
 
     def insert(self, *args):
-        # Validate that the provided row data is correct according to the
-        # columns configuration.
-        # If there's any error, raise ValidationError exception.
-        # Otherwise, serialize the row as a string, and write to to the
-        # table's JSON file.
-        pass
+        rows_dict = {}
+        if len(args) != len(self.columns):
+            raise ValidationError('Invalid amount of field')
 
+        for i, col in enumerate(self.columns):
+            if col['type'] == type(args[i]).__name__:
+                rows_dict[col['name']] = args[i]
+            else:
+                raise ValidationError('Invalid type of field "{}": Given "{}", expected "{}"'.format(col['name'],type(args[i]).__name__, col['type']))
+        
+        self._write_to_file(rows_dict)
+    
+    def _write_to_file(self, row):
+        with open(self.table_filepath, 'r+') as fp:
+            json_dict = json.load(fp)
+            json_dict['rows'].append(row)
+            fp.seek(0)
+            fp.write(json.dumps(json_dict, default=self._serialize_date))
+            
     def query(self, **kwargs):
         # Read from the table's JSON file all the rows in the current table
         # and return only the ones that match with provided arguments.
-        # We would recomment to  use the `yield` statement, so the resulting
-        # iterable object is a generator.
 
-        # IMPORTANT: Each of the rows returned in each loop of the generator
-        # must be an instance of the `Row` class, which contains all columns
-        # as attributes of the object.
-        pass
+        with open(self.table_filepath, 'r') as fp:
+            table_dict = json.load(fp)
+            
+            for row in table_dict['rows']:
+                if all([row[k] == v for k,v in kwargs.items()]): 
+                    yield Row(row)
 
     def all(self):
         # Similar to the `query` method, but simply returning all rows in
         # the table.
-        # Again, each element must be an instance of the `Row` class, with
-        # the proper dynamic attributes.
-        pass
+        with open(self.table_filepath, 'r') as fp:
+            for row in json.load(fp)['rows']:
+                yield Row(row)
+    
 
     def count(self):
         # Read the JSON file and return the counter of rows in the table
-        pass
+        with open(self.table_filepath) as fp:
+            return len(json.load(fp)['rows'])
+            
 
     def describe(self):
         # Read the columns configuration from the JSON file, and return it.
-        pass
-
+        return self.columns
+        
+    def _serialize_date(self, arg):
+        if isinstance(arg, date):
+            return arg.isoformat()
 
 class DataBase(object):
     def __init__(self, name):
         self.name = name
         self.db_filepath = os.path.join(BASE_DB_FILE_PATH, self.name)
         self.tables = self._read_tables()
+        
 
     @classmethod
     def create(cls, name):
         db_filepath = os.path.join(BASE_DB_FILE_PATH, name)
         # if the db directory already exists, raise ValidationError
         # otherwise, create the proper db directory
+        if os.path.exists(db_filepath):
+            raise ValidationError('Database with name "{}" already exists.'.format(name))
+        os.makedirs(db_filepath)
 
     def _read_tables(self):
         # Gather the list of tables in the db directory looking for all files
         # with .json extension.
-        # For each of them, instatiate an object of the class `Table` and
-        # dynamically assign it to the current `DataBase` object.
-        # Finally return the list of table names.
-        # Hint: You can use `os.listdir(self.db_filepath)` to loop through
-        #       all files in the db directory
-        pass
+        file_list = []
+        for file in os.listdir(self.db_filepath):
+            if file.endswith(".json"):
+                file_list.append(file.replace('.json', ''))
+            
+            for f in file_list:
+                setattr(self, f,Table(db=self, name=f))
+        return file_list
+
 
     def create_table(self, table_name, columns):
         # Check if a table already exists with given name. If so, raise
         # ValidationError exception.
-        # Otherwise, crete an instance of the `Table` class and assign
-        # it to the current db object.
-        # Make sure to also append it to `self.tables`
-        pass
+
+        if table_name in self.tables:
+            raise ValidationError("Table with name '{}' already exists".format(table_name))
+        table = Table(db=self,name=table_name, columns=columns)
+        self.tables.append(table_name)
+        setattr(self, table_name, table)
+        
 
     def show_tables(self):
-        # Return the curren list of tables.
-        pass
+        # Return the current list of tables.
+        return self.tables
 
 
 def create_database(db_name):
